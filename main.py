8403e8eb2ad8417cdfc3ec92a9c6b55ee429195e
DCR10 <david.castro.rodriguez@gmail.com>
2020-09-14 23:37:15 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from classes.MyGUI import *
+
+def main():
+    MyWindow=Tk()
+    titleWindow='Mi Diccionario'
+    size='850x860'
+    myWindow = MyGUI(MyWindow,titleWindow,size)
+    MyWindow.mainloop() 
+
+
+main()
+
+
